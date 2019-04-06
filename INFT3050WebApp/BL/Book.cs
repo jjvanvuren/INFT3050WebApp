dd0b00f5c559a104ff4b62ee990c8f8ba67cf0ea
jjvanvuren <jacques.vanvuren@tutanota.com>
2019-04-06 13:44:24 +1100

    Created StoreItem & Book classes

---
+++ INFT3050WebApp/BL/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace INFT3050WebApp.BL
+{
+    public class Book : StoreItem
+    {
+        public string Isbn { get; set; }
+        public DateTime DatePublished { get; set; }
+        public string Title { get; set; }
+        public string SecondaryTitle { get; set; }
+    }
+}
