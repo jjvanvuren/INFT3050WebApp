e8c70d0bd11946d65ef1f0b6498407291db2da15
Karl Foley <karl.k.foley@gmail.com>
2019-04-11 09:41:17 +1000

    Fixed icon link delete unused page normal form pdf and draw.io files

--- INFT3050WebApp/UL/Admin/AdminUserAccounts.aspx.cs
+++ INFT3050WebApp/UL/Admin/AdminUserAccounts.aspx.cs
@@ -11,10 +11,10 @@ namespace INFT3050WebApp.UL.Admin
     {
         void Page_PreInit(object sender, EventArgs e)
         {
-            if (Session["UserSession"] == null)
-            {
-                Response.Redirect("~/UL/Admin/AdminLogin.aspx");
-            }
+            //if (Session["UserSession"] == null)
+            //{
+              //  Response.Redirect("~/UL/Admin/AdminLogin.aspx");
+            //}
 
         }
         protected void Page_Load(object sender, EventArgs e)
@@ -30,24 +30,9 @@ namespace INFT3050WebApp.UL.Admin
 
         }
 
-        /*https://www.codeproject.com/Articles/53559/Accessing-a-DropDownList-inside-a-GridView */
-        /*protected void GridView1_RowCommand(object sender, GridViewRowEventArgs e)
-        {
-            Control ctrl = e.Row.FindControl("DdlIsAdmin");
-            if (ctrl != null)
-            {
-                DropDownList dd = ctrl as DropDownList;
-            }
-        }
-
-        protected void ddlIsAdmin_SelectedIndexChanged(object sender, EventArgs e)
+        protected void ItemManagment_RowUpdated(object sender,GridViewUpdatedEventArgs e)
         {
 
         }
-        /* protected void ddlIsAdmin_SelectedIndexChanged(Object sender, EventArgs e)
-         {
-
-             DdlIsAdmin.SelectedIndex
-         }*/
     }
 }
