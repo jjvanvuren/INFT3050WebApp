4e1fa467c7d7621b9324de3d30bc8edebf7d73e0
Francois Janse van Vuren <c3185723@uon.edu.au>
2019-05-24 12:17:35 +1000

    Added a GetPostageOptions method

--- INFT3050WebApp/BL/PostageOption.cs
+++ INFT3050WebApp/BL/PostageOption.cs
@@ -10,5 +10,6 @@ namespace INFT3050WebApp.BL
         public int Id { get; set; }
         public string Name { get; set; }
         public double Price { get; set; }
+        public bool IsActive { get; set; }
     }
 }
